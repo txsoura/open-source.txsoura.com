9039c3c95bc052b677ab5be62df5b8ea01d0492f
Victor Tesoura Júnior <txsoura@yahoo.com>
2024-03-21 21:22:51 -0300

    feat: set contact social media links

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/txsoura",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@txsoura",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:open-source@txsoura.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "opensource@txsoura.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/txsoura",
 		label: "Github",
-		handle: "chronark",
+		handle: "txsoura",
 	},
 ];
 
